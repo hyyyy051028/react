3474f04db82e9ddbf22531d97bd655d9ac5e454b
hy <2167313615@qq.com>
2024-10-22 00:00:24 +0800

    增加了计时器的案例

--- my-vue-app/src/01-ref引用值.jsx
+++ my-vue-app/src/01-ref引用值.jsx
@@ -17,14 +17,35 @@
 //   );
 // }
 // export default App;
+// import { useState, useRef } from 'react';
+// function App() {
+//   const [count, setCount] = useState(0);
+//   let num = useRef(0);
+//   const handleClick = () => {
+//     setCount(count + 1);
+//     num.current++;
+//     console.log(num.current);
+//   };
+//   return (
+//     <div>
+//       hello App
+//       <button onClick={handleClick}>计数</button>
+//       <br />
+//       {count}
+//     </div>
+//   );
+// }
+// export default App;
 import { useState, useRef } from 'react';
 function App() {
   const [count, setCount] = useState(0);
-  let num = useRef(0);
+  const timer = useRef(null);
   const handleClick = () => {
     setCount(count + 1);
-    num.current++;
-    console.log(num.current);
+    clearInterval(timer.current);
+    timer.current = setInterval(() => {
+      console.log(123);
+    }, 1000);
   };
   return (
     <div>

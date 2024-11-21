bfa13a6ea5e5001cbde88a51904b7a88382af122
hy <2167313615@qq.com>
2024-11-21 16:08:31 +0800

    结束学习，开始复习

--- gaoji/src/main.jsx
+++ gaoji/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './10_react-spring动画库.jsx';
+import App from './09_tailwindcss.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
